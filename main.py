083827128a83de0576b036b5321c335a45cc22f1
Uros lindic <uros.lindic@gmail.com>
2021-10-10 21:34:16 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+firstNum = int(input("Vnesi prvo stevilo: "))
+secondNum = int(input("Vnesi drugo stevilo: "))
+operation = input("Izberi operacijo +,-,*,/ : ")
+
+if operation == "+":
+    print(firstNum+secondNum)
+if operation == "-":
+    print(firstNum-secondNum)
+if operation == "*":
+    print(firstNum*secondNum)
+if operation == "/":
+    print(firstNum/secondNum)
+
